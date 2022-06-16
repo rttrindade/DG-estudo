e9c397e05439cdac72cb09fea31d421c0d0d4cdd
rttrindade <rttrinndade93@gmail.com>
2022-06-16 16:58:32 -0300

    adicionado ciclo de jogo, classe jogador e validação das entradas(corrige o bug de mostrar a porta de entrada como saida)

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ CordX = 0
 CordY = 0
 local = [CordX, CordY]
 
-def OndeEstou(Cord):
-    print("Você esta em:", Cord[0], ",", Cord[1])
+def OndeEstou(Cord, passos):
+    print("Você esta em: "+ str(Cord[0])+ ", "+ str(Cord[1])+ "\nAinda resta: "+ str(passos)+ " passo(s)")
+
+def validaEntrada(entrada):
+    total = 5
+    entrada = total - entrada
+    if entrada == 4:
+        return 1
+    if entrada == 3:
+        return 0
+    if entrada == 2:
+        return 3
+    if entrada == 1:
+        return 2
 
 def Saidas(entrada=1):
     saidas = []
@@ -19,8 +31,6 @@ class SALA():
     def __init__(self, local, caminhos):
         self.cordenada = local
         self.caminhos = caminhos
-     #   self.MostraOpc()
-     #   self.JogadorEscolhe()
 
     def MostraOpc(self,saidas):
         texto = "Pra onde deseja ir?\n"
@@ -36,19 +46,55 @@ class SALA():
 
     def JogadorEscolhe(self,texto):
         direc = input(texto)
-        if direc == '1':
+        global local
+        if direc == '1' and self.caminhos[0]:
+            local[1] += 1
             print("Você foi pro Norte")
-        elif direc == '2':
+            return 1
+        elif direc == '2' and self.caminhos[1]:
+            local[1] -= 1
             print("Você foi pro Sul")
-        elif direc == '3':
+            return 2
+        elif direc == '3' and self.caminhos[2]:
+            local[0] += 1
             print("Você foi pro Leste")
-        elif direc == '4':
+            return 3
+        elif direc == '4' and self.caminhos[3]:
+            local[0] -= 1
             print("Você foi pro Oeste")
+            return 4
+        else:
+            print("Caminho bloqueado, deve haver outra saida.")
+            return -1
 
 
 
+class JOGADOR():
+    def __init__(self):
+        self.trajeto = []
+        self.ultimoMov =1
 
-sala1 = SALA([0,0],Saidas())
-print(sala1.caminhos)
-sala1.JogadorEscolhe(sala1.MostraOpc(sala1.caminhos))
+    def setUltimoMov(self, movimento):
+        self.ultimoMov = movimento
+
+    def getUltimoMov(self):
+        return self.ultimoMov
+
+##############################################################################################################
+
+jogador = JOGADOR()
+passos = 5
+while passos > 0:
+    sala1 = SALA(local, Saidas(validaEntrada(jogador.getUltimoMov())))
+    OndeEstou(local,passos)
+    moveu = False
+    while moveu != True:
+        jogador.setUltimoMov(sala1.JogadorEscolhe(sala1.MostraOpc(sala1.caminhos)))
+        if jogador.getUltimoMov() != -1:
+            passos -= 1
+            moveu = True
+
+
+print("GAME OVER")
+
 #print(sala1.MostraOpc(sala1.caminhos))
